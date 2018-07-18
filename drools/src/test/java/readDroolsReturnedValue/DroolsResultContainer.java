6a80f2ed91343363e0ed013eeecb7814d3198d17
wangxichun <wangxichun@jd.com>
2018-07-18 09:34:59 +0800

    1. 添加drools模块。 2. drools中使用新加Global变量

---
+++ drools/src/test/java/readDroolsReturnedValue/DroolsResultContainer.java
@@ -0,0 +1,23 @@
+package readDroolsReturnedValue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by wangxichun on 2018/7/17.
+ */
+
+
+public class DroolsResultContainer {
+    Map<String,Object> container = new HashMap<String, Object>();
+
+
+    public void putValueByKey(String key, Object value) {
+        container.put(key, value);
+    }
+
+    public Object getValueByKey(String key) {
+        return container.get(key);
+    }
+
+}
---
+++ drools/src/test/java/readDroolsReturnedValue/JavaReadDroolsReturnedValueTest.java
@@ -0,0 +1,44 @@
+package readDroolsReturnedValue;
+
+import model.Product;
+import org.drools.StatefulSession;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import pl.maciejwalkowiak.drools.DroolsJUnitRunner;
+import pl.maciejwalkowiak.drools.annotations.DroolsFiles;
+import pl.maciejwalkowiak.drools.annotations.DroolsSession;
+
+import java.util.Date;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+
+/**
+ * Created by wangxichun on 2018/7/17.
+ */
+@RunWith(DroolsJUnitRunner.class)
+@DroolsFiles(value = "test-rules.drl", location = "/test-rules/")
+public class JavaReadDroolsReturnedValueTest {
+    @DroolsSession
+    StatefulSession kieSession;
+
+    /**
+     * 通过全局变量的方式， 读取返回值。
+     */
+    @Test
+    public void read() {
+        // Java - prior to fireAllRules
+        DroolsResultContainer resultContainer = new DroolsResultContainer();
+        kieSession.setGlobal("resultContainer", resultContainer);
+
+        Product product = new Product();
+
+        kieSession.insert(product);
+        kieSession.fireAllRules();
+
+        assertEquals(150000, product.getDiscount());
+//        assertTrue(product == resultContainer.getValueByKey("k1"));
+//        assertTrue(resultContainer.getValueByKey("k1") instanceof Date);
+    }
+}
