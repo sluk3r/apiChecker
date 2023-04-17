057cce8a40396a6f461a19bd11486f0e7d81a8a4
wangxichun <wangxichun@jd.com>
2023-04-17 14:29:29 +0800

    重构， 方便修改连接信息

--- gpt/src/main/java/ping/gpt/ChatGptDemo.java
+++ gpt/src/main/java/ping/gpt/ChatGptDemo.java
@@ -1,5 +1,6 @@
 package ping.gpt;
 
+
 import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
 import cn.hutool.json.JSONObject;
@@ -9,52 +10,55 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * @author taoxi
- * @version 1.0
- * @description:
- * @date 2023/4/8 8:54 上午
- */
+
 public class ChatGptDemo {
 
+    private static final String URL = "https://service-0ctbjjhm-1317703753.hk.apigw.tencentcs.com/v1/completions";
+    private static final String PRIVATE_TOKEN = "XXXX";
+
     public static void main(String[] args) {
-        Map<String,String> headers = new HashMap<String,String>();
-        headers.put("Content-Type","application/json;charset=UTF-8");
+        ChatGpt chatGpt = new ChatGpt();
+        String responseText = chatGpt.getResponseText("在中国一个25岁的男生应该有多少存款？");
+        System.out.println("GPT text: " + responseText);
+    }
 
-        JSONObject json = new JSONObject();
-        //选择模型
-        json.set("model","text-davinci-003");
-        //添加我们需要输入的内容
-        json.set("prompt","在中国一个25岁的男生应该有多少存款？");
-        json.set("temperature",0.9);
-        json.set("max_tokens",2048);
-        json.set("top_p",1);
-        json.set("frequency_penalty",0.0);
-        json.set("presence_penalty",0.6);
+    private static class ChatGpt {
+        private final Map<String, String> headers;
+        private final JSONObject json;
 
-        String privateToken = "XXXX";
+        public ChatGpt() {
+            headers = new HashMap<>();
+            headers.put("Content-Type", "application/json;charset=UTF-8");
 
+            json = new JSONObject();
+            // 选择模型
+            json.set("model", "text-davinci-003");
+            json.set("temperature", 0.9);
+            json.set("max_tokens", 2048);
+            json.set("top_p", 1);
+            json.set("frequency_penalty", 0.0);
+            json.set("presence_penalty", 0.6);
+        }
 
-//        HttpResponse response = HttpRequest.post("https://api.openai.com/v1/completions")
+        public String getResponseText(String prompt) {
+            // 添加我们需要输入的内容
+            json.set("prompt", prompt);
 
-        HttpResponse response = HttpRequest.post("https://service-0ctbjjhm-1317703753.hk.apigw.tencentcs.com/v1/completions")
-                .headerMap(headers, false)
-                .bearerAuth(privateToken)
-                .body(String.valueOf(json))
-                .timeout(5 * 60 * 1000)
-                .execute();
+            HttpResponse response = HttpRequest.post(URL)
+                    .headerMap(headers, false)
+                    .bearerAuth(PRIVATE_TOKEN)
+                    .body(String.valueOf(json))
+                    .timeout(5 * 60 * 1000)
+                    .execute();
 
+            String body = response.body();
+            System.out.println("========================GPT返回的原始报文=============================================");
+            System.out.println("GPT body: " + body);
 
-        String body = response.body();
-        System.out.println("========================GPT返回的原始报文=============================================");
-        System.out.println("GPT body: " + body);
-
-        Map result = JSON.parseObject(body, Map.class);
-        List choices = (List) result.get("choices");
-        Map o = (Map) choices.get(0);
-        Object text = o.get("text");
-
-        System.out.println("============================================================================");
-        System.out.println("GPT text: " + text);
+            Map<String, Object> result = JSON.parseObject(body, Map.class);
+            List<Map<String, Object>> choices = (List<Map<String, Object>>) result.get("choices");
+            Map<String, Object> o = choices.get(0);
+            return (String) o.get("text");
+        }
     }
 }
